0982319c85b6bfac79fcf700448a25b536c3fc62
ca-trindade <piratum.core@gmail.com>
2023-11-02 10:03:43 +0000

    fetch the temperament and description inside page

--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -1,6 +1,7 @@
-import { fetchTemperamentDescription } from "../../api/axios";
+import axios from "axios";
+import { ENDPOINT } from "../../utils/url";
 import { useEffect, useState } from "react";
-import { Loading } from "../../components/Meowdulating"
+import { Loading } from "../../components/Meowdulating";
 import { Navbar } from "../../components/Navbar";
 import {
   ButtonContainer,
@@ -14,22 +15,17 @@ import {
 } from "./style";
 
 export const Catalog = () => {
-  const [description, setDescription] = useState([]);
-  const [temperament, setTemperament] = useState([]);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState([]);
 
   useEffect(() => {
-    async function getCatalogData() {
-      try {
-        const tempDescrData = await fetchTemperamentDescription();
-        setDescription(tempDescrData.description);
-        setTemperament(tempDescrData.temperament);
-        setLoading(false)
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getCatalogData();
+    axios
+      .get(ENDPOINT.breeds)
+      .then((res) => {
+        setLoading(false);
+        setData(res.data);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   return (
@@ -39,38 +35,31 @@ export const Catalog = () => {
         <GoBack to={"/"}>Choose other Expert</GoBack>
       </ButtonContainer>
 
-      {loading ? (<Loading />) : (
+      {loading ? (
+        <Loading />
+      ) : (
         <>
-      <Title>Affection Alchemist</Title>
-      <ContainerList>
-        <CardContainer>
-          <div>
-            <SecondaryTitle>Description:</SecondaryTitle>
-            <Paragraph>
-              {description}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates obcaecati esse accusamus explicabo porro, distinctio
-              molestias et perferendis natus, id consequuntur aliquam rem
-              dignissimos odit animi, repellendus nemo repudiandae quidem!
-            </Paragraph>
-          </div>
-          <div>
-            <SecondaryTitle>Temperament:</SecondaryTitle>
-            <Paragraph>
-              {temperament}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Blanditiis fugit natus quae molestias ad corrupti, earum
-              aspernatur nesciunt eveniet illum fuga recusandae suscipit
-              perferendis deserunt illo ipsa nostrum officia dolorem?
-            </Paragraph>
-            <ButtonContainer>
-              <FullProfile>Full Profile</FullProfile>
-            </ButtonContainer>
-          </div>
-        </CardContainer>
+          <Title>Affection Alchemist</Title>
+          <ContainerList>
+            {data.map((data) => (
+              <CardContainer key={data.id}>
+                <div>
+                  <SecondaryTitle>Description:</SecondaryTitle>
+                  <Paragraph>{data.description}</Paragraph>
+                </div>
+
+                <div>
+                  <SecondaryTitle>Temperament:</SecondaryTitle>
+                  <Paragraph>{data.temperament}</Paragraph>
+                </div>
+                <ButtonContainer>
+                  <FullProfile to={"/LoginPage"}>Full Profile</FullProfile>
+                </ButtonContainer>
+              </CardContainer>
+            ))}
           </ContainerList>
-          </>
-        )}
+        </>
+      )}
     </>
   );
 };
--- src/pages/Catalog/style.js
+++ src/pages/Catalog/style.js
@@ -36,12 +36,16 @@ export const Title = styled.h2`
 `;
 
 export const ContainerList = styled.ul`
+  list-style-type: none;
   display: flex;
-  justify-content: center;
-  margin: 15px;
+  width: 90vw;
+  flex-wrap: wrap;
+padding: 0;
+  justify-content: space-evenly;
+  margin: auto;
 `;
 
-export const CardContainer = styled.li`
+export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -49,6 +53,7 @@ export const CardContainer = styled.li`
   border-radius: 12px;
   background-color: #eec373;
   padding: 10px 25px 20px 25px;
+  margin: 15px 0;
 `;
 
 export const SecondaryTitle = styled.h3`
@@ -58,7 +63,7 @@ export const SecondaryTitle = styled.h3`
   font-family: "Playfair Display", serif;
 `;
 
-export const Paragraph = styled.p`
+export const Paragraph = styled.li`
   color: #876445;
   font-size: 1vw;
   font-weight: 600;
@@ -76,7 +81,6 @@ export const FullProfile = styled(Link)`
   padding: 5px 20px;
   text-decoration: none;
   margin-top: 10px;
-
 
   &:hover {
     color: #876445;
